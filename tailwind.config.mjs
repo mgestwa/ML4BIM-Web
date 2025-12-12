bb918fc958cbda236eb355a3148f64e9b4cf788b
mgestwa <gestwa.mateusz@gmail.com>
2025-12-12 08:44:54 +0100

    Initial commit - Astro with Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				mono: ['JetBrains Mono', 'monospace'],
+			},
+			colors: {
+				bim: '#F59E0B', // Orange
+				ml: '#06B6D4',  // Cyan
+				dark: '#0B1120', // Main Background
+				card: '#161F32', // Card Background
+			}
+		},
+	},
+	plugins: [],
+}
